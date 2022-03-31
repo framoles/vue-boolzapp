923f53eb506c5bc1b77c87df21dfea609668c0a4
Francesco Moles <francesco.moles@outlook.it>
2022-03-31 15:45:04 +0200

    contacts list ok

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const app = new Vue({
     el: "#vue-app",
     data: {
-
+        contacts,
+        activeIndex: null
     },
     methods: {
-
+        mounted() {
+            console.log(activeIndex);
+        },
+        getSrc(avt) {
+            return "img/avatar" + avt.avatar + ".jpg";
+        },
+        setActive(i) {
+            activeIndex = i;
+            console.log(activeIndex);
+        }
     }
 })
